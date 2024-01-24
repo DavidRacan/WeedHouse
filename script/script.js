8eaff2c6270b8942c6d146908d63bf55dcb65875
DavidRacan <113090931+DavidRacan@users.noreply.github.com>
2024-01-24 09:42:51 +0100

    Update script.js69

--- script/script.js
+++ script/script.js
@@ -1,24 +1,28 @@
 console.log(`Funguje!`);
 
-// 12. Zapojení prvků náhody pomocí matematických funkcí
+const hod6 = () => {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+const hod20 = () => {
+    return Math.floor(Math.random() * 20 + 1);
+}
+
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 window.alert(`Náhodné číslo: ${randomNumber}`);
 
-// 13. Zacyklení událostí na základě daného počtu kroků
-const steps = 5; // Počet kroků
+const steps = 5;
 for (let i = 0; i < steps; i++) {
     window.alert(`Krok číslo: ${i + 1}`);
 }
 
-// 14. Zacyklení událostí na základě výstupní podmínky
 let count = 0;
 const maxCount = 3;
 while (count < maxCount) {
-   window.alert(`Stále podmínka: ${count}`);
+    window.alert(`Stále podmínka: ${count}`);
     count++;
 }
 
-// 15. Ladění vstupů uživatele (zjednodušení kontroly)
 function checkAge(age) {
     const MINIMUM = 15;
 
@@ -39,23 +43,45 @@ const vekUzivatele = window.prompt('Kolik ti je let?', 69);
 const message = checkAge(vekUzivatele);
 window.alert(message);
 
-// 16. Rozšíření vybraných proměnných na pole
 const creators = ['Džon', 'Advy', 'Dejvis'];
 window.alert(`Tvůrci: ${creators.join(', ')}`);
 
 console.log(`Rare jako supreme drop`);
 const studenti = ['Anna', 'Kateřina', 'Marie', 'Pepa'];
 studenti.forEach(element => {
-  studenti[studenti.indexOf(element)] = element.substring(0, 3) + 'ítko';
+    studenti[studenti.indexOf(element)] = element.substring(0, 3) + 'ítko';
 });
 let vybraniStudenti = [];
 
+const hody = Number(window.prompt('Kolikrát chceš hodit kostkou?'));
+const strany = Number(window.prompt('Kolikastěnnou kostkou chceš házet?'));
+
+if (hody >= 1 && (strany === 6 || strany === 20)) {
+    // Volba 6stěnné kostky
+    if (strany === 6) {
+        for (let i = 0; i < hody; i++) {
+            const vysledek = hod6();
+            window.alert(`Na 6stěnné kostce padlo číslo ${vysledek}.`);
+        }
+    }
+    // Volba 20stěnné kostky
+    else if (strany === 20) {
+        for (let i = 0; i < hody; i++) {
+            const vysledek = hod20();
+            window.alert(`Na 20stěnné kostce padlo číslo ${vysledek}.`);
+        }
+    }
+} else {
+    window.alert('Neplatný počet hodů nebo nepodporovaný počet stran');
+}
+
 do {
-  const los = Math.floor(Math.random() * studenti.length);
-  const vybranyStudent = studenti[los];
-  if (!vybraniStudenti.includes(vybranyStudent)) {
-    window.alert(`Dnešním mazánkem je: ${vybranyStudent}`);
-    vybraniStudenti.push(vybranyStudent);
-  }
+    const los = Math.floor(Math.random() * studenti.length);
+    const vybranyStudent = studenti[los];
+    if (!vybraniStudenti.includes(vybranyStudent)) {
+        window.alert(`Dnešním mazánkem je: ${vybranyStudent}`);
+        vybraniStudenti.push(vybranyStudent);
+    }
 } while (vybraniStudenti.length < 3);
-window.alert(`Dnešní mazánci jsou: ${vybraniStudenti.join(', ')}.`);
+
+window.alert(`Dnešním mazánci jsou: ${vybraniStudenti.join(', ')}.`);
