d5ecfd7fb6cf27c90c9c57210ae96b0446269e1b
DavidRacan <113090931+DavidRacan@users.noreply.github.com>
2024-02-21 09:22:04 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -84,4 +84,71 @@ do {
     }
 } while (vybraniStudenti.length < 3);
 
+// 16. Rozšíření vybraných proměnných na pole
+const data = {
+    creators: ['Džon', 'Advy', 'Dejvis'],
+    students: ['Anna', 'Kateřina', 'Marie', 'Pepa'],
+    selectedStudents: [],
+    hodKostkou: function(hody, strany) {
+        if (hody >= 1 && (strany === 6 || strany === 20)) {
+            let results = [];
+            for (let i = 0; i < hody; i++) {
+                const result = strany === 6 ? this.hod6() : this.hod20();
+                results.push(result);
+            }
+            return results;
+        } else {
+            return null;
+        }
+    },
+    hod6: function() {
+        return Math.floor(Math.random() * 6 + 1);
+    },
+    hod20: function() {
+        return Math.floor(Math.random() * 20 + 1);
+    },
+    checkAge: function(age) {
+        const MINIMUM = 15;
+        if (age >= MINIMUM) {
+            return "Jsi dostatečně starý.";
+        } else {
+            return "Jsi příliš mladý.";
+        }
+    }
+};
+
+// 17. Iterovat skrz pole pomocí metody ForEach
+data.students.forEach((student, index) => {
+    data.students[index] = student.substring(0, 3) + 'ítko';
+});
+
+// 19. Upravit kód tak, aby na vhodných místech pouze volal funkce
+const hody = Number(window.prompt('Kolikrát chceš hodit kostkou?'));
+const strany = Number(window.prompt('Kolikastěnnou kostkou chceš házet?'));
+
+const results = data.hodKostkou(hody, strany);
+if (results !== null) {
+    results.forEach(result => {
+        window.alert(`Na ${strany}-stěnné kostce padlo číslo ${result}.`);
+    });
+} else {
+    window.alert('Neplatný počet hodů nebo nepodporovaný počet stran');
+}
+
+const vekUzivatele = window.prompt('Kolik ti je let?', 69);
+const message = data.checkAge(vekUzivatele);
+window.alert(message);
+
+// 20. Strukturovat vybraná data do objektu
+do {
+    const los = Math.floor(Math.random() * data.students.length);
+    const vybranyStudent = data.students[los];
+    if (!data.selectedStudents.includes(vybranyStudent)) {
+        window.alert(`Dnešním mazánkem je: ${vybranyStudent}`);
+        data.selectedStudents.push(vybranyStudent);
+    }
+} while (data.selectedStudents.length < 3);
+window.alert(`Dnešním mazánci jsou: ${data.selectedStudents.join(', ')}.`);
+
+
 window.alert(`Dnešním mazánci jsou: ${vybraniStudenti.join(', ')}.`);
